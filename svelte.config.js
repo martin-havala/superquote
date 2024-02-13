c018a67b70c24003136950172c6d4b9a9e1ca50b
Martin Havala <martin.havala@gmail.com>
2024-02-13 19:50:01 +0100

    Fix typescript compilation

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({
 			// default options are shown
